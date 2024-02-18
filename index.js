2b846d577ee39e0b9c540e1d9bc193bbfa487954
NadiaFortune <hello@nadiafortune.co.uk>
2024-02-18 18:51:09 +0000

    rendered badges and finalised generateMarkdown.js

--- index.js
+++ index.js
@@ -1,97 +1,89 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
-const filename = 'UserREADME.md';
-
+const fs = require("fs")
+const path = require("path")
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
+const filename = "UserREADME.md"
 
 // array of questions for user
 const questions = [
-    {
-        type:   "input",
-        name:   "name of project",
-        message:    "What is the name of your project?",
-    },
-    //"Table of Contents" title to go here in output doc
-    {
-        type:   "input",
-        name:   "description",
-        message: "Please provide a short description of your project.",
-    },
-    {
-        type:   "input",
-        name:   "installation",
-        message:   "Please describe how to install your application.", 
-    },
-    {
-        type:   "input",
-        name:   "usage",
-        message:    "Please describe how to use your application.",
-    },
-    {
-        type:   "checkbox",
-        name:   "license",
-        message:    "Please select the licence you'd like to use for this project.",
-        choices:    [
-                        "Apache License 2.0", 
-                        "MIT License", 
-                        "General Public License", 
-                        "Boost Software License", 
-                        "Eclipse Public LIcense", 
-                        "Creative Commons Zero 1.0",
-                        "Other",
-                    ]
-    },
-    {
-        type:   "input",
-        name:   "contributing",
-        message:    "Please name any other contributors to this project. If none, put N/A.",
-    },
-    {
-        type:   "input",
-        name:   "tests",
-        message:    "Please give details on any testing required. If none, put N/A.",
-    },
-    {
-        type:   "input",
-        name:   "github",
-        message:    "What is your GitHub user name? (Please provide the profile link)",
-    },
-    {
-        type:   "input",
-        name:   "email",
-        message:    "What is your email address?",
-    },
-
-];
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  //"Table of Contents" title to go here in output doc
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a short description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please describe how to install your application.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please describe how to use your application.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select the licence you'd like to use for this project.",
+    choices: [
+      "Apache License 2.0",
+      "MIT License",
+      "Boost Software License",
+      "Eclipse Public License",
+      "Creative Commons Zero 1.0",
+      "Other",
+    ],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "Please name any other contributors to this project. If none, put N/A.",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please give details on any testing required. If none, put N/A.",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub user name? (Please provide the profile link)",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+]
 
 // function to write README file
 function writeToFile(filename, data) {
-    fs.writeFile(filename, data, (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-        } else {
-            console.log('Your README file has been created!');
-        }
-    });
+  fs.writeFile(filename, data, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err)
+    } else {
+      console.log("Your README file has been created!")
+    }
+  })
 }
 
 //Promt user to answer questions
-inquirer.prompt(questions)
-    .then(answers => {
-        const { filename, data } = answers;
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers)
 
-writeToFile(filename, data);
-    });
-
-
-
-
-
+  console.log(generateMarkdown(answers))
+  writeToFile(filename, generateMarkdown(answers))
+})
 
 // // function to initialize program
-function init() {
-    }
+function init() {}
 
 // // function call to initialize program
-init();
+init()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,48 @@
 // function to generate markdown for README
-
+function renderBadge(license) {
+  if (license === "Apache License 2.0") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (license === "MIT License") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (license === "Boost Software License") {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  } else if (license === "Eclipse Public License") {
+    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+  } else if (license === "Creative Commons Zero 1.0") {
+    return "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"
+  }
+    else if (license === "Other") {
+      return "No License Selected"
+    }
+  
+}
 
 function generateMarkdown(data) {
   return `
+  ${renderBadge(data.license)}
   # ${data.title}
 
   ## Table of Contents
 
-  [Description](#description)
-  [Installation](#installation)
-  [Usage](#usage)
-  [License](#license)
-  [Contributors](#contributing)
-  [Testing](#tests)
-  [Questions](#email)
+  [Description](#description)\n
+  [Installation](#installation)\n
+  [Usage](#usage)\n
+  [License](#license)\n
+  [Contributors](#contributors)\n
+  [Testing](#testing)\n
+  [Questions](#questions)\n
 
   ## Description
   ${data.description}
 
   ## Installation
-  ${data.usage}
+  ${data.installation}
 
   ## Usage
   ${data.usage}
 
   ## License
-  ${data.license}
+  ${data.license}  ${renderBadge(data.license)}
 
   ## Contributors
   ${data.contributing}
@@ -37,8 +54,7 @@ function generateMarkdown(data) {
   
   For any questions, please get in touch by email: ${data.email}, or via Github
   ${data.github}
-`;
+`
 }
 
-
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
